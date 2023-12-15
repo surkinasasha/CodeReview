a5e95585ebfd9be8857eddac80fc4cd2c7143b5b
surkinasasha <139327592+surkinasasha@users.noreply.github.com>
2023-12-15 19:28:48 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,20 +1,22 @@
 import sqlite3
-from my_parser import articles
+from my_parser import sum_articles
+from config import DATABASE
+articles = sum_articles()
 def create_database():
-    conn = sqlite3.connect("mydata2.db")
+    conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     sql ="CREATE TABLE articles (title TEXT, link TEXT, date TEXT)"
     cursor.execute(sql)
     conn.close()
 def clear_database():
-    conn = sqlite3.connect("mydata2.db")
+    conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     sql = "DELETE FROM articles"
     cursor.execute(sql)
     conn.commit()
     conn.close()
 def fill_database():
-    conn = sqlite3.connect("mydata2.db")
+    conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     cursor.executemany("INSERT INTO articles VALUES (?, ?, ?)", articles)
     conn.commit()
