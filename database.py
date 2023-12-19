c83c093b8524ae2b0f4ea0f3ec25b7f965433f33
surkinasasha <139327592+surkinasasha@users.noreply.github.com>
2023-12-19 21:19:16 +0500

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ articles = sum_articles()
 def create_database():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
-    sql ="CREATE TABLE IF NOT EXISTS articles (title TEXT, link TEXT, date TEXT)"
+    sql ="CREATE TABLE IF NOT EXISTS articles (title TEXT, link TEXT, date TEXT, PRIMARY KEY (title , link))"
     cursor.execute(sql)
     conn.commit()
     conn.close()
@@ -19,6 +19,6 @@ def clear_database():
 def fill_database():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
-    cursor.executemany("INSERT INTO articles VALUES (?, ?, ?)", articles)
+    cursor.executemany("INSERT OR IGNORE INTO articles VALUES (?, ?, ?)", articles)
     conn.commit()
     conn.close()
